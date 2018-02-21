eb249b7522e4e3cd0b36b09095cfcf0bf328a6d4
ingesolvoll <inge.solvoll@gmail.com>
2018-02-21 09:28:27 +0100

    Whitespace

--- src/cljs/kee_frame_sample/views.cljs
+++ src/cljs/kee_frame_sample/views.cljs
@@ -1,7 +1,6 @@
 (ns kee-frame-sample.views
   (:require [re-frame.core :as re-frame]
-            [kee-frame-sample.subs :as subs]
-            ))
+            [kee-frame-sample.subs :as subs]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
