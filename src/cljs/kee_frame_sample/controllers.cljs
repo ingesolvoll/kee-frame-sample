5957348512049d466578a41632192913574ec7c9
ingesolvoll <inge.solvoll@gmail.com>
2018-02-22 10:34:42 +0100

    Friends don't let friends separate by type of code

--- src/cljs/kee_frame_sample/controllers.cljs
+++
@@ -1,1 +0,0 @@
-(ns kee-frame-sample.controllers)
--- src/cljs/kee_frame_sample/events.cljs
+++
@@ -1,7 +0,0 @@
-(ns kee-frame-sample.events
-  (:require [re-frame.core :as re-frame]))
-
-(re-frame/reg-event-db
-  ::initialize-db
-  (fn [_ _]
-    {}))
