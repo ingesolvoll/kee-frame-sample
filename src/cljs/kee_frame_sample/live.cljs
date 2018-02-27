34a049956d6032632983c15a4c56a337518c24d6
ingesolvoll <inge.solvoll@gmail.com>
2018-02-27 22:44:27 +0100

    Live results page

---
+++ src/cljs/kee_frame_sample/live.cljs
@@ -0,0 +1,23 @@
+(ns kee-frame-sample.live
+  (:require [re-interval.core :refer [register-interval-handlers]]
+            [re-frame.core :refer [reg-event-fx reg-event-db reg-sub debug]]
+            [ajax.core :as ajax]
+            [kee-frame.core :refer [reg-controller]]))
+
+(register-interval-handlers :live nil 1000)
+
+(reg-controller :live
+                {:params (fn [{:keys [handler]}]
+                           (when (= handler :live) true))
+                 :start  [:live/start]
+                 :stop   [:live/stop]})
+
+(reg-event-fx :live/tick
+              (fn [_ _]
+                {:http-xhrio {:method          :get
+                              :uri             (str "http://api.football-data.org/v1/fixtures")
+                              :params          {:timeFrame :n1}
+                              :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
+                              :on-failure      [:log-error]
+                              :response-format (ajax/json-response-format)
+                              :on-success      [:live/fixtures-loaded]}}))
