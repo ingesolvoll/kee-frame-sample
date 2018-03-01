75729a7014dda34667a5be08c2b5e3684fb7e2c9
ingesolvoll <inge.solvoll@gmail.com>
2018-03-01 10:57:30 +0100

    Chain example is getting somewhere

--- src/cljs/kee_frame_sample/league.cljs
+++ src/cljs/kee_frame_sample/league.cljs
@@ -1,19 +1,20 @@
 (ns kee-frame-sample.league
-  (:require [kee-frame.core :refer [reg-controller]]
+  (:require [kee-frame.core :refer [reg-controller] :as k]
             [re-frame.core :refer [reg-event-fx reg-event-db reg-sub debug]]
             [ajax.core :as ajax]))
 
 #_(reg-event-chain :league/load
                    [:fx {:http-xhrio {:method          :get
-                                      :uri             (str "http://api.football-data.org/v1/competitions/" [:event/param 0] "/fixtures")
-                                      :params          {:timeFrame :p7}
+                                      :uri             (str "http://api.football-data.org/v1/competitions/" [::k/params 1] "/fixtures")
+                                      :params          {:timeFrame         :p7
+                                                        :some-other-filter [::k/ctx :db :filter :filter-type]}
                                       :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
-                                      :on-failure      [:log-error]
+                                      :on-failure      [::k/failure] ;; or just another shared [:your-handler-event]
                                       :response-format (ajax/json-response-format)
-                                      :on-success      [:chain/next]}}]
-                   [:fx {:navigate-to "/mordi"
-                         :db          [[:table [:chain/params 0]]
-                                       #(assoc % :dings :boms)]}])
+                                      :on-success      [::k/next]}}]
+                   [:db [[:table [::k/params 1]]]]
+                   [:failure {:navigate-to "/error/page"
+                              :db          [:error :message (str "oh-noes, did not end well that one: " [::k/params 1])]}])
 
 (reg-controller :league
                 {:params (fn [{:keys [handler route-params]}]
