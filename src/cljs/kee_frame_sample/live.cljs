3437f2de113baa5df3b7c4753388cb6b4e8e226e
ingesolvoll <inge.solvoll@gmail.com>
2018-03-03 15:47:35 +0100

    Using chain

--- src/cljs/kee_frame_sample/live.cljs
+++ src/cljs/kee_frame_sample/live.cljs
@@ -1,8 +1,9 @@
 (ns kee-frame-sample.live
+  (:require-macros [kee-frame.chain :refer [reg-chain]])
   (:require [re-interval.core :refer [register-interval-handlers]]
             [re-frame.core :refer [reg-event-fx reg-event-db reg-sub debug]]
             [ajax.core :as ajax]
-            [kee-frame.core :refer [reg-controller]]))
+            [kee-frame.core :refer [reg-controller] :as k]))
 
 ;; Registers events `:live/start` and `:live/stop` events.
 ;; We need to implement `:live/tick` ourselves, it will be called every 1000 ms.
@@ -14,19 +15,15 @@
                  :start  [:live/start]
                  :stop   [:live/stop]})
 
-(reg-event-fx :live/tick
-              (fn [_ _]
-                {:http-xhrio {:method          :get
+(reg-chain :live/tick
+           [:fx {:http-xhrio {:method          :get
                               :uri             (str "http://api.football-data.org/v1/fixtures")
                               :params          {:timeFrame :n1}
                               :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
                               :on-failure      [:log-error]
                               :response-format (ajax/json-response-format)
-                              :on-success      [:live/fixtures-loaded]}}))
+                              :on-success      [::k/next]}}]
+           [:db [[:live-matches [::k/params 0]]]])
 
-(reg-event-db :live/fixtures-loaded
-              [debug]
-              (fn [db [_ league]]
-                (assoc db :live-matches league)))
 
 (reg-sub :live-matches (fn [db] (some-> db :live-matches)))
