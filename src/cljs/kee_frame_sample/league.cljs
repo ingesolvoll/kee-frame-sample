c07baf10b7586ece1785de137f8ae20bf6e71b62
ingesolvoll <inge.solvoll@gmail.com>
2018-03-03 15:44:55 +0100

    Renamed reg-event-chain

--- src/cljs/kee_frame_sample/league.cljs
+++ src/cljs/kee_frame_sample/league.cljs
@@ -1,5 +1,5 @@
 (ns kee-frame-sample.league
-  (:require-macros [kee-frame.chain :refer [reg-event-chain]])
+  (:require-macros [kee-frame.chain :refer [reg-chain]])
   (:require [kee-frame.core :refer [reg-controller] :as k]
             [re-frame.core :refer [reg-event-fx reg-event-db reg-sub debug]]
             [ajax.core :as ajax]))
@@ -10,26 +10,26 @@
                              route-params))
                  :start  (fn [_ {:keys [id tab]}]
                            (case tab
-                             "table" [:league/load-table-chain id]
-                             "fixtures" [:league/load-fixtures-chain id]))})
+                             "table" [:league/load-table id]
+                             "fixtures" [:league/load-fixtures id]))})
 
-(reg-event-chain :league/load-fixtures-chain
-                 [:fx {:http-xhrio {:method          :get
-                                    :uri             (str "http://api.football-data.org/v1/competitions/" [::k/params 0] "/fixtures")
-                                    :params          {:timeFrame :p7}
-                                    :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
-                                    :response-format (ajax/json-response-format)
-                                    :on-success      [::k/next]}}]
-                 [:db [[:fixtures [::k/params 0]]]])
+(reg-chain :league/load-fixtures
+           [:fx {:http-xhrio {:method          :get
+                              :uri             (str "http://api.football-data.org/v1/competitions/" [::k/params 0] "/fixtures")
+                              :params          {:timeFrame :p7}
+                              :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
+                              :response-format (ajax/json-response-format)
+                              :on-success      [::k/next]}}]
+           [:db [[:fixtures [::k/params 0]]]])
 
-(reg-event-chain :league/load-table-chain
-                 [:fx {:http-xhrio {:method          :get
-                                    :uri             (str "http://api.football-data.org/v1/competitions/" [::k/params 0] "/leagueTable")
-                                    ;:params          {:matchday 3}
-                                    :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
-                                    :response-format (ajax/json-response-format)
-                                    :on-success      [::k/next]}}]
-                 [:db [[:table [::k/params 0]]]])
+(reg-chain :league/load-table
+           [:fx {:http-xhrio {:method          :get
+                              :uri             (str "http://api.football-data.org/v1/competitions/" [::k/params 0] "/leagueTable")
+                              ;:params          {:matchday 3}
+                              :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
+                              :response-format (ajax/json-response-format)
+                              :on-success      [::k/next]}}]
+           [:db [[:table [::k/params 0]]]])
 
 (reg-sub :table (fn [db] (some-> db :table)))
 (reg-sub :fixtures (fn [db] (some-> db :fixtures)))
--- src/cljs/kee_frame_sample/leagues.cljs
+++ src/cljs/kee_frame_sample/leagues.cljs
@@ -1,5 +1,5 @@
 (ns kee-frame-sample.leagues
-  (:require-macros [kee-frame.chain :refer [reg-event-chain]])
+  (:require-macros [kee-frame.chain :refer [reg-chain]])
   (:require [kee-frame.core :refer [reg-controller] :as k]
             [re-frame.core :refer [reg-event-fx reg-fx reg-event-db reg-sub debug]]
             [ajax.core :as ajax]))
@@ -13,13 +13,13 @@
                 {:db          (dissoc db :fixtures :table)
                  :navigate-to [:league :id league-id :tab :table]}))
 
-(reg-event-chain :leagues/load
-                 [:fx {:http-xhrio {:method          :get
+(reg-chain :leagues/load
+           [:fx {:http-xhrio {:method          :get
                                     :uri             "http://api.football-data.org/v1/competitions/?season=2017"
                                     :headers         {"X-Auth-Token" "974c0523d8964af590d3bb9d72b45d0a"}
                                     :on-failure      [:log-error]
                                     :response-format (ajax/json-response-format)
                                     :on-success      [::k/next]}}]
-                 [:db [[:leagues [::k/params 0]]]])
+           [:db [[:leagues [::k/params 0]]]])
 
 (reg-sub :leagues (fn [db] (:leagues db)))
