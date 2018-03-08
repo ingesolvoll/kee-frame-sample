f89e18d51af02ff428f277cdcea6ef03b213325c
ingesolvoll <inge.solvoll@gmail.com>
2018-03-08 18:21:55 +0100

    Always hide drawer when navigating to new url

--- src/cljs/kee_frame_sample/events.cljs
+++ src/cljs/kee_frame_sample/events.cljs
@@ -1,7 +1,12 @@
 (ns kee-frame-sample.events
-  (:require [re-frame.core :refer [reg-event-fx reg-event-db debug]]))
+  (:require [re-frame.core :refer [reg-event-fx reg-event-db debug]]
+            [kee-frame.core :refer [reg-controller]]))
 
 (reg-event-db :toggle-drawer
               [debug]
               (fn [db [_ flag]]
                 (assoc db :drawer-open? flag)))
+
+(reg-controller :hide-drawer-on-navigate
+                {:params identity
+                 :start [:toggle-drawer false]})
