c07ee8d55bcb6f2aef9ef289717a48da650055f4
ingesolvoll <inge.solvoll@gmail.com>
2018-03-24 11:10:03 +0100

    Trying latest cljs beta

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kee-frame-sample "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/clojurescript "1.10.217"]
                  [reagent "0.8.0-alpha2"]
                  [re-frame "0.10.5"]
                  [kee-frame "0.1.8-SNAPSHOT"]
