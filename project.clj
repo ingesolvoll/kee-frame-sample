e50c9a3fa82acc72b0e8b5e34f2e1cb865b48234
ingesolvoll <inge.solvoll@gmail.com>
2018-04-01 11:01:29 +0200

    Back to a working setup

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject kee-frame-sample "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
+                 [reagent "0.8.0-alpha2"]
                  [re-frame "0.10.5"]
                  [kee-frame "0.1.8-SNAPSHOT"]
                  [day8.re-frame/http-fx "0.1.5"]
@@ -37,10 +38,8 @@
 
              :dev     {:dependencies [[binaryage/devtools "0.9.4"]
                                       [etaoin "0.2.2"]
-                                      [day8.re-frame/re-frame-10x "0.3.0-1-react16"]
-                                      ;[day8.re-frame/tracing "0.5.0"]
-                                      ]
-                       :plugins      [[lein-figwheel "0.5.15"]]}
+                                      [day8.re-frame/re-frame-10x "0.3.0-1-react16"]]
+                       :plugins      [[lein-figwheel "0.5.13"]]}
 
              :uberjar {:prep-tasks  ["compile" ["cljsbuild" "once" "min"]]
                        :hooks       []
@@ -59,14 +58,12 @@
                                :parallel-build       true
                                :preloads             [devtools.preload day8.re-frame-10x.preload]
                                :closure-defines      {"re_frame.trace.trace_enabled_QMARK_" true
-                                                      ;"day8.re_frame.tracing.trace_enabled_QMARK_" true
                                                       kee-frame-sample.core/debug           true}
                                :external-config      {:devtools/config {:features-to-install :all}}}}
 
                {:id           "min"
                 :source-paths ["src/cljs"]
                 :compiler     {:output-to      "resources/public/js/compiled/app.js"
-                               :aot-cache      true
                                :optimizations  :advanced
                                :parallel-build true}}]}
   :aliases {"integration-test" ["with-profile" "test" "do" ["cljsbuild" "once" "min"] ["test"]]})
