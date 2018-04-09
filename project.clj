742768bbac87d9baa01427295cef3ef0ccbc0d14
ingesolvoll <inge.solvoll@gmail.com>
2018-04-10 00:20:38 +0200

    Next snapshot

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  [org.clojure/clojurescript "1.10.238"]
                  [reagent "0.8.0-alpha2"]
                  [re-frame "0.10.5"]
-                 [kee-frame "0.2.0-SNAPSHOT"]
+                 [kee-frame "0.2.1-SNAPSHOT"]
                  [funcool/bide "1.6.0"]
                  [keechma/router "0.1.1"]
                  [metosin/reitit-core "0.1.0"]
