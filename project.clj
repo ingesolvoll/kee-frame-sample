4221b2c767b2985b3462dd0f6256d3e2210d3145
ingesolvoll <inge@semion.ca>
2020-05-11 10:12:54 +0200

    Switched to figwheel-main

--- project.clj
+++ project.clj
@@ -1,27 +1,18 @@
 (defproject kee-frame-sample "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.597"]
+                 [org.clojure/clojurescript "1.10.758"]
                  [kee-frame "0.4.1-SNAPSHOT"]
                  [reagent "0.8.1"]
                  [re-frame "0.10.6" :exclusions [reagent]]
-                 [funcool/bide "1.6.0"]
-                 [bidi "2.1.3"]
-                 [keechma/router "0.1.1"]
                  [day8.re-frame/http-fx "0.1.5"]
-                 [ring "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
-                 [compojure "1.5.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]
                  [cljs-react-material-ui "0.2.48"]
-                 [com.andrewmcveigh/cljs-time "0.5.2"]
-                 [org.clojure/test.check "0.10.0-alpha2"]]
+                 [com.andrewmcveigh/cljs-time "0.5.2"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
 
   :min-lein-version "2.5.3"
 
-  :source-paths ["src/clj"]
+  :source-paths ["src/cljs"]
 
   :test-paths ["test"]
 
@@ -31,41 +22,25 @@
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
-  :figwheel {:css-dirs     ["resources/public/css"]
-             :ring-handler kee-frame-sample.server/app}
-
-  :profiles {:test    {:dependencies [[etaoin "0.2.2"]]
-                       :plugins      [[com.jakemccrary/lein-test-refresh "0.22.0"]]
+  :profiles {:server  {:source-paths ["src/clj"]
+                       :dependencies [[ring "1.7.1"]
+                                      [ring/ring-defaults "0.2.1"]
+                                      [compojure "1.6.0"]
+                                      [environ "1.0.0"]
+                                      [ring/ring-jetty-adapter "1.7.1"]]}
+             :test    {:source-paths ["test"]
+                       :dependencies [[etaoin "0.3.6"]]
+                       :prep-tasks   ["cljs:prod"]
+                       :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]]
                        :test-refresh {:refresh-dirs ["src/clj" "test"]}}
 
-             :dev     {:dependencies [[binaryage/devtools "1.0.0"]
-                                      [etaoin "0.2.2"]
-                                      [day8.re-frame/re-frame-10x "0.3.3-react16"]]
-                       :plugins      [[lein-figwheel "0.5.16"]]}
+             :dev     {:dependencies [[com.bhauman/figwheel-main "0.2.4"]
+                                      [day8.re-frame/re-frame-10x "0.6.4"]]}
 
-             :uberjar {:prep-tasks  ["compile" ["cljsbuild" "once" "min"]]
-                       :hooks       []
+             :uberjar {:prep-tasks  ["compile" "cljs:prod"]
                        :omit-source true
                        :aot         :all}}
-
-  :cljsbuild {:builds
-              [{:id           "dev"
-                :source-paths ["src/cljs"]
-                :figwheel     true
-                :compiler     {:main                 kee-frame-sample.core
-                               :output-to            "resources/public/js/compiled/app.js"
-                               :output-dir           "resources/public/js/compiled/out"
-                               :asset-path           "/js/compiled/out"
-                               :source-map-timestamp true
-                               :parallel-build       true
-                               :preloads             [devtools.preload day8.re-frame-10x.preload]
-                               :closure-defines      {"re_frame.trace.trace_enabled_QMARK_" true
-                                                      kee-frame-sample.core/debug           true}
-                               :external-config      {:devtools/config {:features-to-install :all}}}}
-
-               {:id           "min"
-                :source-paths ["src/cljs"]
-                :compiler     {:output-to      "resources/public/js/compiled/app.js"
-                               :optimizations  :advanced
-                               :parallel-build true}}]}
-  :aliases {"integration-test" ["with-profile" "test" "do" ["cljsbuild" "once" "min"] ["test"]]})
+  :aliases {"integration-test" ["with-profile" "server" "test"]
+            "figwheel"         ["trampoline" "run" "-m" "figwheel.main" "-b" "dev"]
+            "cljs:prod"        ["run" "-m" "cljs.main" "-co" "prod.cljs.edn" "-c"]
+            "uberjar"          ["with-profile" "uberjar,server" "uberjar"]})
--- src/cljs/kee_frame_sample/routers.cljs
+++ src/cljs/kee_frame_sample/routers.cljs
@@ -1,81 +1,82 @@
 (ns kee-frame-sample.routers
-  (:require [reitit.core :as reitit]
+  #_(:require [reitit.core :as reitit]
             [kee-frame.api :as api]
             [router.core :as keechma]
             [bide.core :as bide]
             [clojure.string :as str]
             [bidi.bidi :as bidi]))
 
-(defrecord BideRouter [routes]
-  api/Router
-  (data->url [_ data] (apply bide/resolve routes data))
-  (url->data [_ url] (let [[handler route-params] (bide/match routes url)]
-                       {:handler      handler
-                        :route-params route-params})))
+(comment
+ (defrecord BideRouter [routes]
+   api/Router
+   (data->url [_ data] (apply bide/resolve routes data))
+   (url->data [_ url] (let [[handler route-params] (bide/match routes url)]
+                        {:handler      handler
+                         :route-params route-params})))
 
-(defrecord KeechmaRouter [routes]
-  api/Router
-  (data->url [_ data] (keechma/map->url routes data))
-  (url->data [_ url] (let [a (keechma/url->map routes url)]
-                       (println a)
-                       a)))
+ (defrecord KeechmaRouter [routes]
+   api/Router
+   (data->url [_ data] (keechma/map->url routes data))
+   (url->data [_ url] (let [a (keechma/url->map routes url)]
+                        (println a)
+                        a)))
 
-(defrecord ReititRouter [routes]
-  api/Router
-  (data->url [_ data] (apply reitit/match-by-name routes data))
-  (url->data [_ url] (reitit/match-by-path routes url)))
+ (defrecord ReititRouter [routes]
+   api/Router
+   (data->url [_ data] (apply reitit/match-by-name routes data))
+   (url->data [_ url] (reitit/match-by-path routes url)))
 
 
-(defn assert-route-data [data]
-  (when-not (vector? data)
-    (throw (ex-info "Bidi route data is a vector consisting of handler and route params as kw args"
-                    {:route-data data}))))
+ (defn assert-route-data [data]
+   (when-not (vector? data)
+     (throw (ex-info "Bidi route data is a vector consisting of handler and route params as kw args"
+                     {:route-data data}))))
 
-(defn url-not-found [routes data]
-  (throw (ex-info "Could not find url for the provided data"
-                  {:routes routes
-                   :data   data})))
+ (defn url-not-found [routes data]
+   (throw (ex-info "Could not find url for the provided data"
+                   {:routes routes
+                    :data   data})))
 
-(defn route-match-not-found [routes url]
-  (throw (ex-info "No match for URL in routes"
-                  {:url    url
-                   :routes routes})))
+ (defn route-match-not-found [routes url]
+   (throw (ex-info "No match for URL in routes"
+                   {:url    url
+                    :routes routes})))
 
-(defrecord BidiBrowserRouter [routes]
-  api/Router
-  (data->url [_ data]
-    (assert-route-data data)
-    (or (apply bidi/path-for routes data)
-        (url-not-found routes data)))
-  (url->data [_ url]
-    (if-let [match (bidi/match-route routes url)]
-      (assoc match :path url)
-      (route-match-not-found routes url))))
+ (defrecord BidiBrowserRouter [routes]
+   api/Router
+   (data->url [_ data]
+     (assert-route-data data)
+     (or (apply bidi/path-for routes data)
+         (url-not-found routes data)))
+   (url->data [_ url]
+     (if-let [match (bidi/match-route routes url)]
+       (assoc match :path url)
+       (route-match-not-found routes url))))
 
-(defrecord BidiHashRouter [routes]
-  api/Router
-  (data->url [_ data]
-    (assert-route-data data)
-    (or (str "/#" (apply bidi/path-for routes data))
-        (url-not-found routes data)))
-  (url->data [_ url]
-    (let [[path+query fragment] (-> url (str/replace #"^/#" "") (str/split #"#" 2))
-          [path query] (str/split path+query #"\?" 2)]
-      (if-let [match (bidi/match-route routes path)]
-        (assoc match :path url
-                     :query-string query
-                     :hash fragment)
-        (route-match-not-found routes url)))))
+ (defrecord BidiHashRouter [routes]
+   api/Router
+   (data->url [_ data]
+     (assert-route-data data)
+     (or (str "/#" (apply bidi/path-for routes data))
+         (url-not-found routes data)))
+   (url->data [_ url]
+     (let [[path+query fragment] (-> url (str/replace #"^/#" "") (str/split #"#" 2))
+           [path query] (str/split path+query #"\?" 2)]
+       (if-let [match (bidi/match-route routes path)]
+         (assoc match :path url
+                      :query-string query
+                      :hash fragment)
+         (route-match-not-found routes url)))))
 
-(def bide-routes
-  (bide/router [["/" :live]
-                ["/league/:id/:tab" :league]]))
+ (def bide-routes
+   (bide/router [["/" :live]
+                 ["/league/:id/:tab" :league]]))
 
-(def keechma-routes
-  [["/:handler" {:handler :live}]
-   "/:handler/:id/:tab"])
+ (def keechma-routes
+   [["/:handler" {:handler :live}]
+    "/:handler/:id/:tab"])
 
-(def reitit-routes
-  (reitit/router
+ (def reitit-routes
+   (reitit/router
     [["/" :live]
-     ["/league/:id/:tab" :league]]))
+     ["/league/:id/:tab" :league]])))
