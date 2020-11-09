39e7f49392bbc2ade3051a1df882a6c0b0056899
ingesolvoll <inge@semion.ca>
2020-11-09 09:47:31 +0100

    Exponential backoff

--- src/cljs/kee_frame_sample/controller/live.cljs
+++ src/cljs/kee_frame_sample/controller/live.cljs
@@ -4,22 +4,39 @@
             [kee-frame-sample.util :as util]
             [re-chain.core :as chain]
             [re-frame.core :as f]
+            [statecharts.core :as sc]
             [taoensso.timbre :as log]))
 
-(def retry-in-10-secs {:after [{:delay  10000
-                                :target ::loading}]})
+(defn calculate-backoff
+  "Exponential backoff, with a upper limit of 15 seconds."
+  [state & _]
+  (-> (js/Math.pow 2 (:retries state))
+      (* 1000)
+      (min 15000)))
+
+(defn update-retries [state & _]
+  (update state :retries inc))
+
+(defn reset-retries [state & _]
+  (assoc state :retries 0))
+
+(def error-loop {:entry (sc/assign update-retries)
+                 :after [{:delay  calculate-backoff
+                          :target ::loading}]})
 
 (def live-fsm
   {:id      :live
    :initial ::initializing
    :states  {::initializing {:initial ::loading
-                             :states  {::error   retry-in-10-secs
+                             :states  {::error   error-loop
                                        ::loading {:entry #(f/dispatch [:live/load-live-matches])
                                                   :on    {:live/loaded-live-matches [:> ::running]
                                                           ::error                   ::error}}}}
              ::running      {:initial ::waiting
-                             :states  {::error   retry-in-10-secs
-                                       ::waiting retry-in-10-secs
+                             :states  {::error   error-loop
+                                       ::waiting {:entry (sc/assign reset-retries)
+                                                  :after [{:delay  10000
+                                                           :target ::loading}]}
                                        ::loading {:entry #(f/dispatch [:live/load-live-matches])
                                                   :on    {:live/loaded-live-matches ::waiting
                                                           ::error                   ::error}}}}}})
