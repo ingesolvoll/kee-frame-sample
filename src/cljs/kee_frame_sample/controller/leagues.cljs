9110f579bbf8702559bb0d4268285c5f1221e36d
ingesolvoll <inge@semion.ca>
2020-11-12 09:31:25 +0100

    New HTTP FSM system

--- src/cljs/kee_frame_sample/controller/leagues.cljs
+++ src/cljs/kee_frame_sample/controller/leagues.cljs
@@ -1,37 +1,23 @@
 (ns kee-frame-sample.controller.leagues
   (:require [kee-frame.core :as k]
-            [kee-frame.fsm.alpha :as fsm]
             [kee-frame-sample.util :as util]
-            [re-chain.core :as chain]
             [re-frame.core :as f]))
 
-(def leagues-fsm
-  {:id      :leagues
-   :initial ::loading
-   :states  {::loading        {:entry #(f/dispatch [:leagues/load])
-                               :on    {::loaded ::loaded
-                                       ::error  ::loading-failed}}
-             ::loading-failed {:after [{:delay  3000
-                                        :target ::loading}]
-                               :on    {:leagues/retry ::loading}}
-             ::loaded         {}}})
+(def leagues-request-fsm {:id          :leagues
+                          :http-xhrio  (util/http-get "https://api.football-data.org/v/competitions")
+                          :max-retries 5
+                          :on-success  ::loaded})
 
 (k/reg-controller :leagues
                   {:params (constantly true)
                    :start  (fn []
-                             leagues-fsm)})
+                             leagues-request-fsm)})
 
 ;; Only show the most interesting ones, with compatible data
 (def whitelist #{2021 2014 2019 2015 2002})
 
-(chain/reg-chain
- :leagues/load
- (fn [_ _]
-   {:http-xhrio (util/http-get "https://api.football-data.org/v2/competitions"
-                               {:on-failure [:leagues/transition ::error]})})
-
- (fn [{:keys [db]} [_ leagues]]
-   {:db       (assoc db :leagues (->> leagues
-                                      :competitions
-                                      (filter (comp whitelist :id))))
-    :dispatch [:leagues/transition ::loaded]}))
+(f/reg-event-db ::loaded
+  (fn [db [_ leagues]]
+    (assoc db :leagues (->> leagues
+                            :competitions
+                            (filter (comp whitelist :id))))))
