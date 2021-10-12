4662a2ee58b6861a117c21df3480ffb9baac6b67
ingesolvoll <inge.solvoll@gmail.com>
2021-10-12 16:12:23 +0200

    Upgraded, reorganized to maybe run on heroku

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject kee-frame-sample "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.597"]
+  :dependencies [[thheller/shadow-cljs "2.15.12"]
                  [kee-frame "1.2.0"]
                  [glimt "0.1.0"]
                  [day8.re-frame/http-fx "0.1.5"]
@@ -23,25 +22,20 @@
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :profiles {:server  {:source-paths ["src/clj"]
-                       :dependencies [[ring "1.7.1"]
-                                      [ring/ring-defaults "0.2.1"]
-                                      [compojure "1.6.0"]
+                       :dependencies [[org.clojure/clojure "1.10.3"]
+                                      [ring "1.9.4"]
+                                      [ring/ring-defaults "0.3.3"]
+                                      [compojure "1.6.2"]
                                       [environ "1.0.0"]
-                                      [kee-frame "1.1.1-SNAPSHOT"]
-                                      [ring/ring-jetty-adapter "1.7.1"]]}
+                                      [ring/ring-jetty-adapter "1.9.4"]]}
              :test    {:source-paths ["test"]
                        :dependencies [[etaoin "0.3.6"]]
                        :prep-tasks   ["cljs:prod"]
                        :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]]
                        :test-refresh {:refresh-dirs ["src/clj" "test"]}}
 
-             :dev     {:dependencies [[com.bhauman/figwheel-main "0.2.8"]
-                                      [day8.re-frame/re-frame-10x "0.6.7"]]}
-
-             :uberjar {:prep-tasks  ["compile" "cljs:prod"]
+             :uberjar {:prep-tasks  ["compile" "cljs"]
                        :omit-source true
                        :aot         :all}}
-  :aliases {"integration-test" ["with-profile" "server" "test"]
-            "figwheel"         ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
-            "cljs:prod"        ["run" "-m" "cljs.main" "-co" "prod.cljs.edn" "-c"]
-            "uberjar"          ["with-profile" "uberjar,server" "uberjar"]})
+  :aliases {"cljs"    ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]
+            "uberjar" ["with-profile" "uberjar,server" "uberjar"]})
