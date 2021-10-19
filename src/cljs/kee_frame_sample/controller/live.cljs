7c55cb087654834da1e57257868c211612fb5587
ingesolvoll <inge.solvoll@gmail.com>
2021-10-19 12:30:16 +0200

    Using interceptor based FSM

--- src/cljs/kee_frame_sample/controller/live.cljs
+++ src/cljs/kee_frame_sample/controller/live.cljs
@@ -9,18 +9,17 @@
 (defn calculate-backoff
   "Exponential backoff, with a upper limit of 15 seconds."
   [retries & _]
-  (-> (js/Math.pow 2 retries)
+  (-> (Math/pow 2 retries)
       (* 1000)
       (min 15000)))
 
 (def live-matches-loader
-  {:transition-event ::transition
-   :http-xhrio       (util/http-get "https://api.football-data.org/v2/matches")
-   :on-success       [:live/loaded-live-matches]
-   :retry-delay      calculate-backoff
-   :max-retries      10
-   :state-path       [:> ::running ::loading]
-   :success-state    [:> ::running ::waiting]})
+  {:http-xhrio    (util/http-get "https://api.football-data.org/v2/matches")
+   :on-success    [:live/loaded-live-matches]
+   :retry-delay   calculate-backoff
+   :max-retries   10
+   :state-path    [:> ::running ::loading]
+   :success-state [:> ::running ::waiting]})
 
 (def live-fsm
   {:id               :live
@@ -29,7 +28,7 @@
    :states           {::running {:initial ::loading
                                  :states  {::waiting {:after [{:delay  10000
                                                                :target ::loading}]}
-                                           ::loading (http/embedded-fsm live-matches-loader)}}}})
+                                           ::loading (fsm/http live-matches-loader)}}}})
 
 (k/reg-controller :live-polling
                   {:params (fn [route]
