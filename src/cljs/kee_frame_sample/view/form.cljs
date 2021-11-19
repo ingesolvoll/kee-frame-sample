2ce732e604294405bc96a764343ca54e3145ef43
ingesolvoll <inge.solvoll@gmail.com>
2021-11-19 12:43:20 +0100

    Demo both FSM modes

--- src/cljs/kee_frame_sample/view/form.cljs
+++ src/cljs/kee_frame_sample/view/form.cljs
@@ -10,21 +10,47 @@
              ::editing {:on {::edit-ended ::dirty}}
              ::dirty   {:on {::edit-started ::editing}}}})
 
+(def global-validation-fsm
+  (-> validation-fsm
+      (with-meta {:open? true})
+      (assoc :id :validation-open)))
+
+(f/reg-event-fx ::edit-started (constantly nil))
+(f/reg-event-fx ::edit-ended (constantly nil))
+
+(defn form-state [state text]
+  (fsm/match-state @state
+                   ::editing [:div "User is editing..."]
+                   ::clean [:div "No changes made yet"]
+                   ::dirty [:div
+                            "Form has been modified and is "
+                            (if (seq @text)
+                              "valid"
+                              [:span {:style {:color :red}} "invalid"])]
+                   nil [:div]))
+
 (defn form []
-  (fsm/with-fsm [state validation-fsm]
-    (r/with-let [text        (r/atom "")
-                 update-text #(reset! text (-> % .-target .-value))]
-      [:div
-       (fsm/match-state @state
-                        ::editing [:div "User is editing..."]
-                        ::clean [:div "No changes made yet"]
-                        ::dirty [:div
-                                 "Form has been modified and is "
-                                 (if (seq @text)
-                                   "valid"
-                                   [:span {:style {:color :red}} "invalid"])]
-                        nil [:div])
-       [:input {:type      :text
-                :on-change #(update-text %)
-                :on-focus  #(f/dispatch [::fsm/transition :validation ::edit-started])
-                :on-blur   #(f/dispatch [::fsm/transition :validation ::edit-ended])}]])))
+  [:div
+   "A form using local events mode"
+   (fsm/with-fsm [state validation-fsm]
+     (r/with-let [text        (r/atom "")
+                  update-text #(reset! text (-> % .-target .-value))]
+       [:div
+        [form-state state text]
+        [:input {:type      :text
+                 :on-change #(update-text %)
+                 :on-focus  #(f/dispatch [::fsm/transition :validation ::edit-started])
+                 :on-blur   #(f/dispatch [::fsm/transition :validation ::edit-ended])}]
+        [:button {:on-click #(f/dispatch [::fsm/restart :validation])} "Reset input FSM"]]))
+
+   "A form using global events mode"
+   (fsm/with-fsm [state global-validation-fsm]
+     (r/with-let [text        (r/atom "")
+                  update-text #(reset! text (-> % .-target .-value))]
+       [:div
+        [form-state state text]
+        [:input {:type      :text
+                 :on-change #(update-text %)
+                 :on-focus  #(f/dispatch [::edit-started :validation-open])
+                 :on-blur   #(f/dispatch [::edit-ended :validation-open])}]
+        [:button {:on-click #(f/dispatch [::fsm/restart :validation-open])} "Reset input FSM"]]))])
